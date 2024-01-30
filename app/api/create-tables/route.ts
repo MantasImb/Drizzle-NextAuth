f0c099cc418e666ac8f51db817d743d85e097006
Mantas Imbrasas <mantasimbrasas@gmail.com>
2024-01-30 11:19:36 +0200

    integrate drizzle, push users table

---
+++ app/api/create-tables/route.ts
@@ -0,0 +1,19 @@
+import { sql } from "@vercel/postgres";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  try {
+    const result = await sql`CREATE TABLE Users ( 
+        id SERIAL PRIMARY KEY, 
+        name VARCHAR(255) NOT NULL, 
+        email VARCHAR(255) NOT NULL UNIQUE, 
+        password VARCHAR(255) NOT NULL, 
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        click_count INTEGER DEFAULT 0,
+        last_click_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+       );`;
+    return NextResponse.json({ result }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
