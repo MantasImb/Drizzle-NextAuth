a3a8b9eb5e62b23ab732e56ea36ad82abde85070
Mantas Imbrasas <mantasimbrasas@gmail.com>
2024-02-01 11:39:23 +0200

    Finalise, fill in README.md

--- README.md
+++ README.md
@@ -4,12 +4,32 @@ Trying out Drizzle and NextAuth.js to test general next.js integration.
 
 ### Set-up:
 
+- Start by setting up a database by following the instructions in the Drizzle docs:
+- - https://orm.drizzle.team/docs/
+- Add NextAuth to the project:
+- - https://authjs.dev/getting-started/
+- - https://authjs.dev/reference/adapter/drizzle
+- Set up the wanted providers. In this case, GitHub was used. Every provider is different, so make sure to read the docs for the provider you want to use.
 - A separate .env file is required for the database connection (Drizzle-kit doesn't read from .env.local)
+- Run `pnpm dbgenerate` to generate the schema
+- Run `pnpm dbpush` to push the schema to the database
+- For the "session" to include the userId, create the @/types/next-auth.d.ts file. This is required for the session to include the userId.
+- - https://authjs.dev/getting-started/typescript
+- A callback to the session is then added in the @/app/auth.ts file to include the userId in the session.
+- Database actions are added in the @/db/actions.ts file, which are then used in the Counter component (@/app/components/counter.tsx).
 
 ### COMMANDS:
 
 - `pnpm dbgenerate` Generates drizzle schema
 - `pnpm dbpush` Pushes generated schema to the db
+
+### Files of interest:
+
+- @/drizzle.config.ts
+- @/.env.example
+- @/types/next-auth.d.ts
+- @/db
+- @/app/api/auth/[...nextauth]/route.ts
 
 ### REFERENCES:
 
--- app/api/create-tables/route.ts
+++ app/api/create-tables/route.ts
@@ -3,24 +3,25 @@ DONT NEED THIS FILE ANYMORE
 */
 
 // import { sql } from "@vercel/postgres";
-// import { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-// export async function GET(request: Request) {
-//   try {
-//     const result = await sql`CREATE TABLE Users (
-//         id SERIAL PRIMARY KEY,
-//         name VARCHAR(255) NOT NULL,
-//         email VARCHAR(255) NOT NULL UNIQUE,
-//         password VARCHAR(255) NOT NULL,
-//         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-//         click_count INTEGER DEFAULT 0,
-//         last_click_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-//        );`;
-//     return NextResponse.json({ result }, { status: 200 });
-//   } catch (error) {
-//     return NextResponse.json({ error }, { status: 500 });
-//   }
-// }
+export async function GET(request: Request) {
+  //   try {
+  //     const result = await sql`CREATE TABLE Users (
+  //         id SERIAL PRIMARY KEY,
+  //         name VARCHAR(255) NOT NULL,
+  //         email VARCHAR(255) NOT NULL UNIQUE,
+  //         password VARCHAR(255) NOT NULL,
+  //         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  //         click_count INTEGER DEFAULT 0,
+  //         last_click_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  //        );`;
+  // return NextResponse.json({ result }, { status: 200 });
+  return NextResponse.json({ hello: "world" }, { status: 200 });
+  //   } catch (error) {
+  //     return NextResponse.json({ error }, { status: 500 });
+  //   }
+}
 
 // import { NextResponse } from "next/server";
 // import { migrate } from "drizzle-orm/vercel-postgres/migrator";
